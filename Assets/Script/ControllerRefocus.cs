72977e9e3926321d8ef0df7605870f8b609985a9
lazydevyo <44928422+lazydevyo@users.noreply.github.com>
2019-01-14 16:30:01 +0200

    Add files via upload

---
+++ Assets/Script/ControllerRefocus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+// If there is no selected item, set the selected item to the event system's first selected item
+public class ControllerRefocus : MonoBehaviour
+{
+	GameObject lastselect;
+	
+	void Start()
+	{
+		lastselect = new GameObject();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (EventSystem.current.currentSelectedGameObject == null)
+		{
+			EventSystem.current.SetSelectedGameObject(lastselect);
+		}
+		else
+		{
+			lastselect = EventSystem.current.currentSelectedGameObject;
+		}
+	}
+	
+}
---
+++ Assets/Script/Documentation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Documentation : MonoBehaviour {
+	public Image[] Pages;
+	public int Page_Int;
+	void Start(){
+		ChangePage ();
+	}
+	public void ChangePage (){
+		int i = 0;
+
+		foreach (Image pag in Pages) {
+		
+			i++;
+			Pages[(i-1)].gameObject.SetActive(false);
+		}
+
+		Pages [Page_Int].gameObject.SetActive (true);
+	}
+
+	public void NextPage(){
+		if (Page_Int == Pages.Length-1) {
+			//Page_Int = 0;
+		} else {
+			Page_Int++;
+		}
+
+		ChangePage ();
+	}
+
+	public void PrewPage(){
+		if (Page_Int == 0) {
+			//Page_Int = Pages.Length-1;
+		} else {
+			Page_Int--;
+		}
+		ChangePage ();
+	}
+
+
+}
